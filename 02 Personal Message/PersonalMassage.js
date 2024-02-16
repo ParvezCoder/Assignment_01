15145a6395abd84e6481ebfb1d8af062efff00dd
Parvez Coder <ParvezCoder786@gmail.com>
2024-02-16 21:01:21 +0500

    First time chacking

---
+++ 02 Personal Message/PersonalMassage.js
@@ -0,0 +1,2 @@
+var Personal_Message = "I am Very thankfull to Mr. Zia Khan, And Our Honourabl Governor Mr. Kamran Khan tessori, And Most respectable person Daniyal Nagori";
+console.log(Personal_Message);
---
+++ 02 Personal Message/personalMassage.ts
@@ -0,0 +1,4 @@
+let Personal_Message = "I am Very thankfull to Mr. Zia Khan, And Our Honourabl Governor Mr. Kamran Khan tessori, And Most respectable person Daniyal Nagori";
+console.log(Personal_Message);
+
+
